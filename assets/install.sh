1c36792071eb6083aea673d46f0e8372042a554a
Caleb <caleb.tutty@nzherald.co.nz>
2015-03-20 16:25:37 +1300

    Install script tweaks

--- assets/install.sh
+++ assets/install.sh
@@ -29,7 +29,20 @@ chmod +x /opt/postfix.sh
 postconf -e myhostname=$maildomain
 postconf -F '*/*/chroot = n'
 
+###############
+#
+################
+
 sed -i "s/localhost.localdomain, , localhost/$maildomain, localhost.localdomain, localhost/" /etc/postfix/main.cf
+
+#####################
+# Alaveteli specific
+#####################
+
+if [ -n "$alaveteli_user" ]; then
+  adduser --quiet --disabled-password \
+   --gecos "Alaveteli User" $alaveteli_user
+fi
 
 
 ################
@@ -41,82 +54,20 @@ if [ -n "$pipescript" ]; then
 
   ### If you want filter by mail prefix
   cat > /etc/postfix/transports <<EOF
-/^fyi.*/                $smtp_user
+/^fyi.*/                $alaveteli_user
+EOF
+
+  cat >> /etc/postfix/master.cf <<EOF
+$alaveteli_user unix  - n n - 50 pipe
+  flags=R user=$alaveteli_user argv=/opt/$pipescript
 EOF
 
   cat >> /etc/postfix/main.cf <<EOF
 transport_maps = regexp:/etc/postfix/transports
-EOF
-  cat >> /etc/postfix/master.cf <<EOF
-alaveteli unix  - n n - 50 pipe
-  flags=R user=$smtp_user argv=/opt/$pipescript
-
-smtp      inet  n       -       -       -       -       smtpd
-        -o content_filter=alaveteli:dummy
+local_recipient_maps = proxy:unix:passwd.byname regexp:/etc/postfix/recipients
 EOF
 
   cat > /etc/postfix/recipients <<EOF
 /^fyi.*/                this-is-ignored
 EOF
 fi
-
-#############
-#  opendkim
-#############
-
-if [[ -z "$(find /etc/opendkim/domainkeys -iname *.private)" ]]; then
-  exit 0
-fi
-cat >> /etc/supervisor/conf.d/supervisord.conf <<EOF
-
-[program:opendkim]
-command=/usr/sbin/opendkim -f
-EOF
-# /etc/postfix/main.cf
-postconf -e milter_protocol=2
-postconf -e milter_default_action=accept
-postconf -e smtpd_milters=inet:localhost:12301
-postconf -e non_smtpd_milters=inet:localhost:12301
-
-cat >> /etc/opendkim.conf <<EOF
-AutoRestart             Yes
-AutoRestartRate         10/1h
-UMask                   002
-Syslog                  yes
-SyslogSuccess           Yes
-LogWhy                  Yes
-
-Canonicalization        relaxed/simple
-
-ExternalIgnoreList      refile:/etc/opendkim/TrustedHosts
-InternalHosts           refile:/etc/opendkim/TrustedHosts
-KeyTable                refile:/etc/opendkim/KeyTable
-SigningTable            refile:/etc/opendkim/SigningTable
-
-Mode                    sv
-PidFile                 /var/run/opendkim/opendkim.pid
-SignatureAlgorithm      rsa-sha256
-
-UserID                  opendkim:opendkim
-
-Socket                  inet:12301@localhost
-EOF
-cat >> /etc/default/opendkim <<EOF
-SOCKET="inet:12301@localhost"
-EOF
-
-cat >> /etc/opendkim/TrustedHosts <<EOF
-127.0.0.1
-localhost
-192.168.0.1/24
-
-*.$maildomain
-EOF
-cat >> /etc/opendkim/KeyTable <<EOF
-mail._domainkey.$maildomain $maildomain:mail:$(find /etc/opendkim/domainkeys -iname *.private)
-EOF
-cat >> /etc/opendkim/SigningTable <<EOF
-*@$maildomain mail._domainkey.$maildomain
-EOF
-chown opendkim:opendkim $(find /etc/opendkim/domainkeys -iname *.private)
-chmod 400 $(find /etc/opendkim/domainkeys -iname *.private)
